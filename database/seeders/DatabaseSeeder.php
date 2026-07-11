72b01e39e48fc651abea983823c067de4272a632
yordanos123-hub <yordina090@gmail.com>
2026-07-11 14:20:57 +0300

    Updated seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,25 +29,39 @@ class DatabaseSeeder extends Seeder
             'phone' => '09-22-22-22-22'
         ]);
         // 3. የጁስ ዝርዝር (16 አይነቶች)
+        // 3. የጁስ ዝርዝር (ከነ ምስላቸው)
         $juiceData = [
-            ['name'=>'Avocado','category'=>'Juice','price'=>150], ['name'=>'Papaya','category'=>'Juice','price'=>150],
-            ['name'=>'Spris','category'=>'Juice','price'=>150], ['name'=>'Ambasha','category'=>'Juice','price'=>150],
-            ['name'=>'Zaytun','category'=>'Juice','price'=>150], ['name'=>'Strawberry','category'=>'Juice','price'=>180],
-            ['name'=>'Mango','category'=>'Juice','price'=>200], ['name'=>'Spris with Mango','category'=>'Juice','price'=>170],
-            ['name'=>'Pineapple','category'=>'Juice','price'=>180], ['name'=>'Watermelon','category'=>'Juice','price'=>160],
-            ['name'=>'Apple','category'=>'Juice','price'=>240], ['name'=>'Orange','category'=>'Juice','price'=>180],
-            ['name'=>'Lemon','category'=>'Juice','price'=>150], ['name'=>'Juice World Special','category'=>'Juice','price'=>220],
-            ['name'=>'Fruit Punch Big','category'=>'Juice','price'=>230], ['name'=>'Fruit Punch Small','category'=>'Juice','price'=>180],
+            ['name'=>'Avocado','category'=>'Juice','price'=>150, 'image'=>'avocado.jpg'],
+            ['name'=>'Papaya','category'=>'Juice','price'=>150, 'image'=>'papaya.jpg'],
+            ['name'=>'Spris','amharic'=>'ስፕሪስ','category'=>'Juice','price'=>150, 'image'=>'spris.jpg'],
+            ['name'=>'Ambasha','category'=>'Juice','price'=>150, 'image'=>'ambasha.jpg'],
+            ['name'=>'Zaytun','category'=>'Juice','price'=>150, 'image'=>'zaytun.jpg'],
+            ['name'=>'Strawberry','category'=>'Juice','price'=>180, 'image'=>'strawberry.jpg'],
+            ['name'=>'Mango','category'=>'Juice','price'=>200, 'image'=>'mango.jpg'],
+            ['name'=>'Spris with Mango','category'=>'Juice','price'=>170, 'image'=>'spris-mango.jpg'],
+            ['name'=>'Pineapple','category'=>'Juice','price'=>180, 'image'=>'pineapple.jpg'],
+            ['name'=>'Watermelon','category'=>'Juice','price'=>160, 'image'=>'watermelon.jpg'],
+            ['name'=>'Apple','category'=>'Juice','price'=>240, 'image'=>'apple.jpg'],
+            ['name'=>'Orange','category'=>'Juice','price'=>180, 'image'=>'orange.jpg'],
+            ['name'=>'Lemon','category'=>'Juice','price'=>150, 'image'=>'lemon.jpg'],
+            ['name'=>'Juice World Special','category'=>'Juice','price'=>220, 'image'=>'special.jpg'],
+            ['name'=>'Fruit Punch Big','category'=>'Juice','price'=>230, 'image'=>'punch-big.jpg'],
+            ['name'=>'Fruit Punch Small','category'=>'Juice','price'=>180, 'image'=>'punch-small.jpg'],
         ];
 
-        // 4. የሚልክሼክ ዝርዝር (11 አይነቶች)
+// 4. የሚልክሼክ ዝርዝር (ከነ ምስላቸው)
         $shakeData = [
-            ['name'=>'Avocado Shake','category'=>'Milkshake','price'=>200], ['name'=>'Papaya Shake','category'=>'Milkshake','price'=>200],
-            ['name'=>'Zaytun Shake','category'=>'Milkshake','price'=>200], ['name'=>'Ambasha Shake','category'=>'Milkshake','price'=>200],
-            ['name'=>'Watermelon Shake','category'=>'Milkshake','price'=>210], ['name'=>'Banana Shake','category'=>'Milkshake','price'=>200],
-            ['name'=>'Mango Shake','category'=>'Milkshake','price'=>250], ['name'=>'Chocolate Shake','category'=>'Milkshake','price'=>220],
-            ['name'=>'Date Shake','category'=>'Milkshake','price'=>250], ['name'=>'Strawberry Shake','category'=>'Milkshake','price'=>250],
-            ['name'=>'Vanilla Shake','category'=>'Milkshake','price'=>200],
+            ['name'=>'Avocado Shake','category'=>'Milkshake','price'=>200, 'image'=>'AvocadoShake.jpg'],
+            ['name'=>'Papaya Shake','category'=>'Milkshake','price'=>200, 'image'=>'PapayaShake.jpg'],
+            ['name'=>'Zaytun Shake','category'=>'Milkshake','price'=>200, 'image'=>'ZaytunShake.jpg'],
+            ['name'=>'Ambasha Shake','category'=>'Milkshake','price'=>200, 'image'=>'AmbashaShake.jpg'],
+            ['name'=>'Watermelon Shake','category'=>'Milkshake','price'=>210, 'image'=>'WatermelonShake.jpg'],
+            ['name'=>'Banana Shake','category'=>'Milkshake','price'=>200, 'image'=>'BananaShake.jpg'],
+            ['name'=>'Mango Shake','category'=>'Milkshake','price'=>250, 'image'=>'MangoShake.jpg'],
+            ['name'=>'Chocolate Shake','category'=>'Milkshake','price'=>220, 'image'=>'ChocolateShake.jpg'],
+            ['name'=>'Date Shake','category'=>'Milkshake','price'=>250, 'image'=>'DateShake.jpg'],
+            ['name'=>'Strawberry Shake','category'=>'Milkshake','price'=>250, 'image'=>'StrawberryShake.jpg'],
+            ['name'=>'Vanilla Shake','category'=>'Milkshake','price'=>200, 'image'=>'VanillaShake.jpg'],
         ];
 
         // ዳታቤዝ ውስጥ አስገባቸው እና ከቅርንጫፍ ጋር አያይዛቸው
